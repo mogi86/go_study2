1e04ba1edae161a0b394f3f631e08d9835eb1ded
mogi86 <nobuyuki.mogi@gmail.com>
2019-07-07 15:25:49 +0900

    modify var name

--- json_schema.go
+++ json_schema.go
@@ -14,17 +14,17 @@ func main() {
 	dataPath := args[1]
 
 	schemaLoader := gojsonschema.NewReferenceLoader(fmt.Sprintf("file://%v", jsonPath))
-	documentLoader := gojsonschema.NewReferenceLoader(fmt.Sprintf("file://%v", dataPath))
+	dataLoader := gojsonschema.NewReferenceLoader(fmt.Sprintf("file://%v", dataPath))
 
-	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	result, err := gojsonschema.Validate(schemaLoader, dataLoader)
 	if err != nil {
 		panic(err.Error())
 	}
 
 	if result.Valid() {
-		fmt.Printf("The document is valid\n")
+		fmt.Printf("The date is valid\n")
 	} else {
-		fmt.Printf("The document is not valid. see errors :\n")
+		fmt.Printf("The date is not valid. see errors :\n")
 		for _, desc := range result.Errors() {
 			fmt.Printf("- %s\n", desc)
 		}
